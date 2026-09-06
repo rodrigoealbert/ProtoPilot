04478aeb645072d7082cb28d86a6a51c6aa0a8df
rodrigoealbert <71835541+rodrigoealbert@users.noreply.github.com>
2026-09-06 15:20:48 +0000

    Fix camera button styling

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+console.log("app.js loaded successfully");
+
+console.log("About to run camera button code");
+
+const cameraAllow = document.querySelector("#cameraAllow");
+
+console.log(cameraAllow);
+
 let cameraStatus = false;
 
 let count = 0;
@@ -5,3 +13,4 @@ let count = 0;
 function cameraRequest() {
     console.log("Camera request button clicked")
 }
+
