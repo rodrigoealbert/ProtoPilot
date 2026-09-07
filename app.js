5e243484bb39bd50eb11675bfba91a7673ffec40
rodrigoealbert <71835541+rodrigoealbert@users.noreply.github.com>
2026-09-07 00:52:48 +0000

    First mediapipe code w/ glitches

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-console.log("app.js loaded successfully");
+import { FilesetResolver, HandLandmarker } from "@mediapipe/tasks-vision";
 
-console.log("About to run camera button code");
+console.log("app.js loaded successfully");
 
 const cameraAllow = document.querySelector("#cameraAllow");
 
-console.log(cameraAllow);
+const cameraStatusH = document.getElementById("cameraStatusH");
+let cameraStatus = "Camera Off";
+let cameraValue = 0;
 
-let cameraStatus = false;
-
-let count = 0;
-
-function cameraRequest() {
+function cameraRequested() {
     console.log("Camera request button clicked")
+    cameraValue = cameraValue+1;
+    console.log("Camera value: " + cameraValue);
+
+    if (cameraValue % 2 === 1) { 
+        cameraStatus = "Camera On";
+    }
+     else {
+        cameraStatus = "Camera Off";
+    }
+        cameraStatusH.textContent = cameraStatus;
+        console.log("Camera status: " + cameraStatus);
 }
+function cameraRequest() {
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function(stream) {
+            var videoElement = document.getElementById('video');
+}           if(videoElement) {
+    videoElement.srcObject = stream;
+    videoElement.play();
+}   
+}).catch(function(err) {
+    console.log("Error accessing camera: " + err);
+}
+else {
+    console.log("Camera not supported"); 
+}
+
+cameraAllow.addEventListener("click", cameraRequest);
 
